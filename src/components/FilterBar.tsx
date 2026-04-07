2be541901e94216c4992986bcd0faf86bed41f0e
Sadorra <rodiawg@gmail.com>
2026-04-07 20:37:07 +0300

    refactor: updated filters to tab based filters.

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -9,17 +9,10 @@ export default function FilterBar({
 }: Props) {
   return (
     <div className="flex gap-4 my-4">
-      <select
-        title="status"
-        value={status}
-        onChange={(e) => setStatus(e.target.value)}
-        className="bg-slate-800 text-white p-2 rounded-lg"
-      >
-        <option value="all">All</option>
-        <option value="unread">Unread</option>
-        <option value="reading">Reading</option>
-        <option value="completed">Completed</option>
-      </select>
+      <button title="status" value={status} onClick={() => setStatus("all")} className="bg-slate-800 text-white p-2 rounded-lg">All</button>
+        <button title="status" value={status} onClick={() => setStatus("reading")} className="bg-slate-800 text-white p-2 rounded-lg">Reading</button>
+        <button title="status" value={status} onClick={() => setStatus("completed")} className="bg-slate-800 text-white p-2 rounded-lg">Completed</button>
+     
     </div>
   );
 }
